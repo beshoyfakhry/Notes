0cc27311d48ee3fa4c3c23576a86ac02f97bf984
BeshoyFakhry <beshoyfakhry@gmail.com>
2023-11-07 00:49:08 +0200

    Revert "adding comment"
    This reverts commit c84d650cf996e8acd89d3bc9e5f0c6e50a038155.

--- app/src/main/java/com/beshoy/notes/MainActivity.kt
+++ app/src/main/java/com/beshoy/notes/MainActivity.kt
@@ -23,7 +23,6 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     Greeting("Android")
-                    //Test
                 }
             }
         }
