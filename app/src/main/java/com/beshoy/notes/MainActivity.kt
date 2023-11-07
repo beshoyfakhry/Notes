8cd66474f202007f77df98b0248a444f4f3df54d
BeshoyFakhry <beshoyfakhry@gmail.com>
2023-11-07 11:03:28 +0200

    second commit

--- app/src/main/java/com/beshoy/notes/MainActivity.kt
+++ app/src/main/java/com/beshoy/notes/MainActivity.kt
@@ -24,6 +24,7 @@ class MainActivity : ComponentActivity() {
                 ) {
                     Greeting("Android")
                     //Comment 1
+                    //Comment 2
                 }
             }
         }
